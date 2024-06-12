c76cf8938836080a1cbece076632771c32f4fa5c
Issa Hassane <issahassane1@hotmail.com>
2024-06-13 04:22:14 +0530

    first commit

---
+++ example/resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Home</x-slot:heading>
+    <h1>{{$greetings}} home page</h1>
+</x-layout>
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+$cars = ['bmw'=> 1,"tata"=>1];
+foreach ($cars as $car => $count){ 
+    echo "$car ";
+    echo $count;
+    echo "\n";
+
+}
+
+
+?>
